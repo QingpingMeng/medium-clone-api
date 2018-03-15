625e886c4f9f1aa9e8f61a88792230c85dd71462
QingpingMeng <missing1989@gmail.com>
2018-03-14 22:04:56 -0700

    add db setting

--- src/common/db.ts
+++ src/common/db.ts
@@ -6,7 +6,8 @@ export const connectToDatabase = () => {
         return Promise.resolve();
     }
 
-    const dbUri: string = process.env.DB || 'string';
+    const defaultDb = `mongodb://test_dev:adminpassword@medium-clone-db-shard-00-00-efkts.mongodb.net:27017,medium-clone-db-shard-00-01-efkts.mongodb.net:27017,medium-clone-db-shard-00-02-efkts.mongodb.net:27017/test?ssl=true&replicaSet=medium-clone-db-shard-0&authSource=admin`;
+    const dbUri: string = process.env.DB || defaultDb;
     return connect(dbUri).then((db: Mongoose) => {
         isConnected = db.connection.readyState == 1; // 1 for connected
     });
