90aaceae72b2407ebf3cce44ec1569b21eb3ed01
QingpingMeng <missing1989@gmail.com>
2018-03-28 21:01:01 -0700

    add feed articles api

--- handler.ts
+++ handler.ts
@@ -9,6 +9,7 @@ export { unfollowUser } from './src/profiles/unfollowUser';
 export { listArticles } from './src/articles/listArticles';
 export { createArticle } from './src/articles/createArticle';
 export { getArticle } from './src/articles/getArticle';
+export { feedArticles } from './src/articles/feedArticles';
 
 require('dotenv').config({ path: './variables.env' });
 
---
+++ src/articles/feedArticles.ts
@@ -0,0 +1,53 @@
+import { Handler, Context, Callback } from 'aws-lambda';
+import { connectToDatabase } from '../common/db';
+import { User } from '../models/User';
+import { Article } from '../models/Article';
+import { validateToken } from '../auth/authorizer';
+
+export const feedArticles: Handler = (
+    event: any,
+    context: Context,
+    callback: Callback
+) => {
+    const limit = event.queryStringParameters.limit || 20;
+    const offset = event.queryStringParameters.offset || 0;
+    console.log(limit, offset);
+    connectToDatabase()
+        .then(() => {
+            return validateToken(event.headers.authorization)
+                .then(decoded => {
+                    return User.findById(decoded.id).exec();
+                })
+                .catch(() => Promise.resolve(undefined));
+        })
+        .then(user => {
+            if (!user) {
+                callback(undefined, {
+                    statusCode: 401
+                });
+                return Promise.reject(new Error('Unauthorized'));
+            }
+
+            console.log(user.following);
+            return Promise.all([
+                Promise.resolve(user),
+                Article.find({ author: { $in: user.following } })
+                    .limit(Number(limit))
+                    .skip(Number(offset))
+                    .populate('author')
+                    .exec(),
+                Article.count({ author: { $in: user.following } }).exec()
+            ]);
+        })
+        .then(([user, articles, articlesCount]) => {
+            return callback(undefined, {
+                statusCode: 200,
+                body: JSON.stringify({
+                    articles: articles.map(article => {
+                        return article.toJSONFor(user);
+                    }),
+                    articlesCount
+                })
+            });
+        });
+};
